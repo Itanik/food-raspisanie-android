dd59a7a2836da8c8e8d33d4633591a65161f42b7
Itanik <yan.xhrome@gmail.com>
2022-02-16 17:29:52 +0300

    Change log format

--- app/src/main/java/com/example/fooduploader/ObservableTimberTree.kt
+++ app/src/main/java/com/example/fooduploader/ObservableTimberTree.kt
@@ -3,14 +3,18 @@ package com.example.fooduploader
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import timber.log.Timber
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ObservableTimberTree : Timber.Tree() {
 
     private var _logLiveData = MutableLiveData("")
     val logLiveData: LiveData<String>
         get() = _logLiveData
+    private val dateFormat = SimpleDateFormat("HH:mm:ss", Locale.ROOT)
 
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
-        _logLiveData.value = message
+
+        _logLiveData.value = dateFormat.format(Calendar.getInstance().time) + " | " + message
     }
 }
