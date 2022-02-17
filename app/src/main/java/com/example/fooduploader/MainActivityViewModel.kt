391bd376a26e4540a90080932cff6b2c20dded28
Itanik <yan.xhrome@gmail.com>
2022-02-17 05:00:13 +0300

    Configure buttons behavior.

--- app/src/main/java/com/example/fooduploader/MainActivityViewModel.kt
+++ app/src/main/java/com/example/fooduploader/MainActivityViewModel.kt
@@ -2,8 +2,10 @@ package com.example.fooduploader
 
 import android.content.res.AssetManager
 import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.RecyclerView
+import com.example.fooduploader.data.Credentials
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import timber.log.Timber
@@ -11,6 +13,13 @@ import timber.log.Timber
 class MainActivityViewModel : ViewModel() {
     private val logger = ObservableTimberTree()
     private lateinit var credentials: Credentials
+
+    private var _status = MutableLiveData<String?>()
+    val status get() = _status
+    private var _selectMenuBtnName = MutableLiveData<String?>()
+    val selectMenuBtnName get() = _selectMenuBtnName
+    private var _selectTableBtnName = MutableLiveData<String?>()
+    val selectTableBtnName get() = _selectTableBtnName
 
     fun initLogger(lifecycleOwner: LifecycleOwner, debugLog: RecyclerView) {
         Timber.plant(logger)
@@ -26,5 +35,17 @@ class MainActivityViewModel : ViewModel() {
             it.readText()
         }
         credentials = Json.decodeFromString(credJson)
+    }
+
+    fun selectMenu() {
+        _status.value = "Выбор меню"
+    }
+
+    fun selectTable() {
+        _status.value = "Выбор таблицы"
+    }
+
+    fun upload() {
+        _status.value = "Загрузка"
     }
 }
